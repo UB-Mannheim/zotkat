4ff34784d359bef3afec0e3ed1c8820d0f56c73c
Philipp Zumstein <zuphilip@gmail.com>
2015-08-01 00:03:04 +0200

    Updated PicaSWB for journalArticles

--- PicaSWB.js
+++ PicaSWB.js
@@ -35,29 +35,78 @@ var item;
 function doExport() {
 
 	while (item = Zotero.nextItem()) {
+		//item.type --> 0500 Bibliographische Gattung und Status
+		//http://swbtools.bsz-bw.de/winibwhelp/Liste_0500.htm
+		Zotero.write( "0500 Aou\n");//TODO abhängig von item.type
+		
+		//item.date --> 1100 
 		var date = Zotero.Utilities.strToDate(item.date);
 		if (date["year"] != undefined) {
 			Zotero.write( "1100 "+ date["year"].toString() +  "\n");
 		}
+		
+		//1130 Datenträger
+		//http://swbtools.bsz-bw.de/winibwhelp/Liste_1130.htm
+		Zotero.write( "1130 \n");
+		
+		//1140 Veröffentlichungsart und Inhalt
+		Zotero.write( "1140 \n");
+		
+		//item.language --> 1500 Sprachcodes
 		if (item.language) { Zotero.write( "1500 " + item.language + "\n"); }
+		
+		//item.ISBN --> 2000 ISBN
 		if (item.ISBN) { Zotero.write( "2000 " + item.ISBN + "\n"); }
+		
+		//item.DOI --> 2051 oder 2053 ???
 		if (item.DOI) { Zotero.write( "2051 " + item.DOI + "\n"); }
+		
+		//Autoren --> 3000ff
+		//Titel, erster Autor --> 4000
+		var titleStatement = "4000 ";
+		if (item.title) { titleStatement += item.title; }
 		var i = 0;
 		while (item.creators.length>0) {
-			var firstCreator = item.creators.shift();
-			Zotero.write( "300"+i+" "+ firstCreator.firstName + " " + firstCreator.lastName + " (" + Zotero.Utilities.getLocalizedCreatorType(firstCreator.creatorType) +")\n" );
-			i++;
+			var creator = item.creators.shift();
+			if (creator.creatorType == "author") {
+				Zotero.write( "300"+i+" " + creator.lastName + ", " + creator.firstName + "\n" );
+				if (i == 0) {
+					titleStatement += "$h" + creator.firstName + " " + creator.lastName;
+				}
+				i++;
+			}
+			//TODO: editors, other contributors...
 		}
-		Zotero.write( "4000 " + item.title + "\n");
+		Zotero.write( titleStatement + "\n");
+		
+		//Ausgabe --> 4020
 		if (item.edition) { Zotero.write( "4020 " + item.edition + "\n"); }
+		
+		//Erscheinungsvermerk --> 4030
 		var publicationStatement = "4030 ";
 		if (item.place) { publicationStatement += item.place; }
 		if (item.publisher) { publicationStatement +=  "$n" + item.publisher; }
 		Zotero.write( publicationStatement +"\n");
-		if (item.url) { Zotero.write( "4085 " + item.url + "$xH"); }
+		
+		//4070 $v Bandzählung $j Jahr $h Heftnummer $p Seitenzahl
+		var volumeyearissuepage = "4070 ";
+		if (item.volume) { volumeyearissuepage += "$v" + item.volume; }
+		if (date["year"] != undefined) { volumeyearissuepage +=  "$j" + date["year"]; }
+		if (item.issue) { volumeyearissuepage += "$h" + item.issue; }
+		if (item.pages) { volumeyearissuepage += "$p" + item.pages; }
+		Zotero.write( volumeyearissuepage +"\n");
+		
+		//URL --> 4085
+		if (item.url) { Zotero.write( "4085 " + item.url + "$xH" + "\n"); }
+		
+		//Reihe --> 4110
 		var seriesStatement = "4110 ";
 		if (item.series) { seriesStatement += item.series; }
 		if (item.seriesNumber) { seriesStatement += " ; " + item.seriesNumber; }
 		Zotero.write(seriesStatement + "\n");
+		
+		//item.publication --> 4241
+		if (item.publication) { Zotero.write( "4241 In: " + item.publication + "\n"); }
+		
 	}
 }
