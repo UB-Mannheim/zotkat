07b0e062a12d0120449d6c3b09eaa93097bf5228
Konstantin Baierer <unixprog@gmail.com>
2016-04-10 00:49:58 +0200

    Replace npm 'test' with a shell script 'jshint.sh', UB-Mannheim#19
    This makes it more readable and avoids confusion with escaping asterisks
    and double quotes

---
+++ jshint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 1st sed: Replace top lines including first beginning with '}' with empty string
+# 2nd sed: Insert newline before every '/** BEGIN TEST'
+# 3rd sed: Delete from first line containing 'BEGIN TEST' to end of file
+# Run the npm-installed jshint
+# No errors -> print 'filename.js OK'
+# Errors -> Set $err to non-zero value
+for f in *.js; do
+      sed '1,/^}/ s/.*//' $f \
+    | sed 's,/\*\* BEGIN TEST,\n\0,' \
+    | sed '/BEGIN TEST/,$ d' \
+    | tee foo.js \
+    | ./node_modules/.bin/jshint --filename=$f - \
+        && echo "$f OK" \
+        || err=1
+done;
+exit $err
