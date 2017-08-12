e3e052e43c0f4db163e5b8050622370a48babc2a
Philipp Zumstein <zuphilip@gmail.com>
2017-08-12 16:20:24 +0200

    WQS: Update test/wqs.js

--- test/wqs.js
+++ test/wqs.js
@@ -27,7 +27,7 @@ var wdq = {
             var date = strToDate(str)
             var iso = false
             if (date.year) {
-                iso = date.year
+                iso = ''+date.year
                 if (date.month) {
                     iso += (date.month < 9 ? '-0' : '-' ) + (1+date.month)
                     if(date.day) {
@@ -49,6 +49,7 @@ var fail = 0
 fs.readdirSync('test').forEach(file => {
     var match = file.match(/(.+)\.json$/)
     if (match) {
+        console.log('\n== UNIT TEST '+count+' ==')
         var name = match[1]
         var item = JSON.parse(fs.readFileSync('test/'+file, 'utf8'))
         var got  = wdq.zoteroItemToQuickStatements(item);
@@ -58,8 +59,11 @@ fs.readdirSync('test').forEach(file => {
         if (got == expect) {
             console.log('ok '+count+' - test/'+file)
         } else {
+            fs.writeFileSync('test/'+name+'.output.txt', got);
             console.log('not ok '+count+' - test/'+file)
             console.log(got)
+            console.log('see test/'+name+'.output.txt for the current output, e.g. with')
+            console.log('git diff --no-index test/'+name+'.output.txt test/'+name+'.txt')
             fail=1
         }
         count++
